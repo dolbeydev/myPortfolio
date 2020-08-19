4b8d2b6ec799d8ab4b0b0572529feb5201f1a161
MenacingBeats <info@menacingbeats.com>
2020-08-19 11:29:20 +1000

    updating portfolio finally

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $fname = $_POST['name'];
+    $lname = $_POST['subject'];
+    $emailfrom = $_POST['email'];
+    $subject = $_POST['subject'];
+
+    $mailTo = 'reid@dolbeydev.com';
+    $headers = "from: ".$emailfrom;
+    $txt = "You have recieved an e-mail from ".$fname.".\n\n".$subject;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
